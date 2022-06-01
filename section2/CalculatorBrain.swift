bec4e05c64f6bafba2dc112ba872c719d686f8fb
sladuf <63739061+sladuf@users.noreply.github.com>
2022-06-01 16:24:56 +0900

    Create CalculatorBrain.swift

---
+++ section2/CalculatorBrain.swift
@@ -0,0 +1,71 @@
+
+
+import Foundation
+
+class CalculatorBrain {
+    
+    private var accumulator = 0.0
+    
+    func setOperand(operand : Double) {
+        accumulator = operand
+        
+    }
+    
+    private var operations = [
+        "π" : Operation.Constant(Double.pi),
+        "e" : Operation.Constant(M_E),
+        "√" : Operation.UnaryOperation(sqrt),
+        "cos" : Operation.UnaryOperation(cos),
+        "×" : Operation.BinaryOperation({ $0*$1 }),
+        "÷" : Operation.BinaryOperation({ $0/$1 }),
+        "+" : Operation.BinaryOperation({ $0+$1 }),
+        "−" : Operation.BinaryOperation({ $0-$1 }),
+        "=" : Operation.Equals
+    ]
+    
+    private enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double,Double) -> Double)
+        case Equals
+    }
+    
+    
+    func performOperation(symbol : String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .Constant(let value) :
+                accumulator = value
+            case .UnaryOperation(let function) :
+                accumulator = function(accumulator)
+            case .BinaryOperation(let function) :
+                excutePendingBinaryOperation()
+                pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+            case .Equals :
+                excutePendingBinaryOperation()
+            }
+        }
+    }
+    
+    private func excutePendingBinaryOperation() {
+        if pending != nil {
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+    
+    
+    private var pending : PendingBinaryOperationInfo?
+    
+    private struct PendingBinaryOperationInfo{
+        var binaryFunction : (Double,Double) -> Double
+        var firstOperand : Double
+    }
+    
+    var result : Double {
+        get {
+            return accumulator
+        }
+    }
+    
+}
